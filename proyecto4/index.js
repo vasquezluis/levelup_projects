795a30f2040c445fbb7df1da275d0ecc617c9678
LuiiVasquez <luivasque95@gmail.com>
2022-11-07 23:40:55 -0600

    changing proyecto4

--- proyecto4/index.js
+++ proyecto4/index.js
@@ -1,8 +1,25 @@
-const poke_txt = document.querySelector(".pokemon_txt");
-const poke_img = document.querySelector(".pokemon_img");
-const card_body = document.querySelector(".card-body");
+const card1 = document.getElementById("card1");
+const card2 = document.getElementById("card2");
+const card3 = document.getElementById("card3");
+const card4 = document.getElementById("card4");
+const card5 = document.getElementById("card5");
+const card6 = document.getElementById("card6");
 
-const main = async () => {
+const pokeCard = (img, titulo, hab1, hab2, hab3, cardId) => {
+  cardId.innerHTML = `
+    <div class="card">
+      <img class="card-img-top" max-width='50%' src="${img}" alt="${titulo}" title="${titulo}" />
+      <div class="card-body text-light bg-secondary">
+        <h3 class="card-title"><strong>${titulo}</strong></h3>
+        <p class="card-text">Habilidad 1: ${hab1}</p>
+        <p class="card-text">Habilidad 2: ${hab2}</p>
+        <p class="card-text">Habilidad 3: ${hab3}</p>
+      </div>
+    </div>
+  `;
+};
+
+const main = async (cardId) => {
   let pokemon = Math.floor(Math.random() * 249);
 
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -10,17 +27,34 @@ const main = async () => {
 
   console.log(body);
 
-  poke_txt.innerText = body.name;
-  poke_img.src = body.sprites["front_default"];
+  let ability1 = body.abilities[0].ability.name;
+  let ability2 = body.abilities[1];
+  let ability3 = body.abilities[2];
 
-  for (let i = 0; i < body.abilities.length; i++) {
-    const parrafo = document.createElement("p");
-    const node = document.createTextNode(
-      `Habilidad ${i + 1}: ` + body.abilities[i].ability.name
-    );
-    parrafo.appendChild(node);
-    card_body.append(parrafo);
+  if (ability2 == null) {
+    ability2 = "Nulo";
+  } else {
+    ability2 = body.abilities[1].ability.name;
   }
+  if (ability3 == null) {
+    ability3 = "Nulo";
+  } else {
+    ability3 = body.abilities[2].ability.name;
+  }
+
+  pokeCard(
+    body.sprites["front_default"],
+    body.name,
+    ability1,
+    ability2,
+    ability3,
+    cardId
+  );
 };
 
-main();
+main(card1);
+main(card2);
+main(card3);
+main(card4);
+main(card5);
+main(card6);
