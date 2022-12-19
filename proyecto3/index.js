742150ce074f40a588d7a80380b9a7ef218f1199
LuiiVasquez <luivasque95@gmail.com>
2022-12-19 11:13:09 -0600

    formating code

--- proyecto3/index.js
+++ proyecto3/index.js
@@ -1,11 +1,7 @@
 const btn_color = document.getElementById("color_btn");
 const txt_color = document.getElementById("color_txt");
 import lista_colores from "./colores.js";
-
 btn_color.addEventListener("click", () => {
   let bgCode = Math.floor(Math.random() * 144);
-
   txt_color.innerHTML = lista_colores[bgCode];
-
-  document.body.style.background = lista_colores[bgCode];
-});
+  document.body.style.background = lista_colores[bgCode];});
