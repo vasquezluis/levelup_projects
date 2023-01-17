0c35960fe8042331f74b6515d97cc535fb6fceb3
LuiiVasquez <luivasque95@gmail.com>
2023-01-17 10:48:20 -0600

    adding delete & updating functions from apiSlice

--- lecciones/redux-toolkit-query/src/components/TasksList.jsx
+++ lecciones/redux-toolkit-query/src/components/TasksList.jsx
@@ -1,7 +1,15 @@
-import { useGetTasksQuery } from "../api/apiSlice";
+import {
+  useGetTasksQuery,
+  useDeleteTasksMutation,
+  useUpdateTasksMutation,
+} from "../api/apiSlice";
 
 function TasksList() {
   const { data: tasks, isError, isLoading, error } = useGetTasksQuery();
+
+  const [deleteTask] = useDeleteTasksMutation();
+
+  const [updateTask] = useUpdateTasksMutation();
 
   if (isLoading) return <div>Cargando...</div>;
   else if (isError) return <div>Error: {error.message}</div>;
@@ -12,8 +20,18 @@ function TasksList() {
         <li key={task.id}>
           <h3>{task.name}</h3>
           <p>{task.description}</p>
-          <button>Delete</button>
-          <input type="checkbox" id={task.id} />
+          <button onClick={() => deleteTask(task.id)}>Delete</button>
+          <input
+            type="checkbox"
+            id={task.id}
+            checked={task.completed}
+            onChange={(e) =>
+              updateTask({
+                ...task,
+                completed: e.target.checked,
+              })
+            }
+          />
           <label htmlFor={task.id}>Completed</label>
         </li>
       ))}
