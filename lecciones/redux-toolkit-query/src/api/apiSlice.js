15f75d3aa679969f157e3db181c0de703284c170
LuiiVasquez <luivasque95@gmail.com>
2023-01-17 10:48:52 -0600

    creating create, update & delete queries

--- lecciones/redux-toolkit-query/src/api/apiSlice.js
+++ lecciones/redux-toolkit-query/src/api/apiSlice.js
@@ -11,8 +11,43 @@ export const apiSlice = createApi({
   endpoints: (builder) => ({
     getTasks: builder.query({
       query: () => "/tasks",
+      providesTags: ["Tasks"],
+      // para transformar la forma de proveer los datos
+      transformResponse: (response) => response.sort((a, b) => b.id - a.id),
+    }),
+    createTasks: builder.mutation({
+      query: (newTask) => ({
+        url: "/tasks",
+        method: "POST",
+        body: newTask,
+      }),
+      // volver a ejecutar la pedida de datos (por medio del nombre: providesTags)
+      // para no tener que recargar la pagina y ver los cambios
+      invalidatesTags: ["Tasks"],
+    }),
+    updateTasks: builder.mutation({
+      query: (updatedTask) => ({
+        url: `/tasks/${updatedTask.id}`,
+        method: "PATCH",
+        body: updatedTask,
+      }),
+      invalidatesTags: ["Tasks"],
+    }),
+    deleteTasks: builder.mutation({
+      // se usa parentesis porque ejecuta la funcion
+      // retorna un objeto
+      query: (id) => ({
+        url: `/tasks/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Tasks"],
     }),
   }),
 });
 
-export const { useGetTasksQuery } = apiSlice;
+export const {
+  useGetTasksQuery,
+  useCreateTasksMutation,
+  useDeleteTasksMutation,
+  useUpdateTasksMutation,
+} = apiSlice;
