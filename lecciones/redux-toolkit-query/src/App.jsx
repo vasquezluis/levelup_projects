9b8376095dde806f2838bb1820fac8a866b0bd5e
LuiiVasquez <luivasque95@gmail.com>
2023-01-17 10:47:50 -0600

    adding Form component

--- lecciones/redux-toolkit-query/src/App.jsx
+++ lecciones/redux-toolkit-query/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import TasksForm from "./components/TasksForm";
 import TasksList from "./components/TasksList";
 
 function App() {
   return (
     <div className="App">
+      <TasksForm />
       <TasksList />
     </div>
   );
